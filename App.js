196c02f00d11059e38d5b4481fe171f46fa20beb
leeyc924 <leeyc924@naver.com>
2021-04-26 23:12:21 +0900

    Setting 모달 컴포넌트 구현

--- App.js
+++ App.js
@@ -1,26 +1,144 @@
 //App.js
-import React from 'react';
-import { StyleSheet,TextInput,  Text, View, Image } from 'react-native';
+import React, {useState} from 'react';
+import {
+  StyleSheet,
+  TextInput,
+  Text,
+  View,
+  Image,
+  TouchableOpacity,
+  ScrollView,
+  ImageBackground,
+} from 'react-native';
+import Setting from './Setting.js';
 
 const App = () => {
+  const [ddayInfo, setDdayInfo] = useState({
+    dday: new Date().getDate(),
+    title: '테스트 디데이',
+  });
+  const [isOpen, setIsOpen] = useState(false);
+  const [chatLog, setChatLog] = useState([]);
+
+  const toggleModal = () => {
+    setIsOpen(isOpen => !isOpen);
+  };
+
+  const settingHandler = (title, date) => {
+    console.log(title, date);
+    setDdayInfo(ddayInfo => ({
+      ...ddayInfo,
+      title: title,
+      dday: date,
+    }));
+    toggleModal();
+  };
+
   return (
     <View style={styles.container}>
-      <Image
-        style={{width: 50, height: 50}}
-        source={{uri: 'https://facebook.github.io/react-native/img/tiny_logo.png'}}
-      />
-      <TextInput/>
-      <Text>
-        Now Loading...
-      </Text>
+      <ImageBackground
+        style={{width: '100%', height: '100%'}}
+        source={require('./images/background.png')}>
+        <View style={styles.settingView}>
+          <TouchableOpacity onPress={toggleModal}>
+            <Image source={require('./icon/setting.png')} />
+          </TouchableOpacity>
+        </View>
+        <View style={styles.ddayView}>
+          <Text style={styles.titleText}>{ddayInfo.title}까지</Text>
+          <Text style={styles.ddayText}>{ddayInfo.dday}</Text>
+          <Text style={styles.dateText}>2020년 11월 32일</Text>
+        </View>
+        <View style={styles.chatView}>
+          <ScrollView style={styles.chatScrollView}></ScrollView>
+          <View style={styles.chatControl}>
+            <TextInput style={styles.chatInput} />
+            <TouchableOpacity style={styles.sendButton}>
+              <Text>전송</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+        {isOpen && (
+          <Setting
+            modalHandler={toggleModal}
+            settingHandler={(title, date) => settingHandler(title, date)}
+          />
+        )}
+      </ImageBackground>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: 'gold',
-},
+    flex: 1,
+  },
+  settingView: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'flex-end',
+    marginRight: '1%',
+  },
+  ddayView: {
+    flex: 6,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  chatView: {
+    flex: 6,
+  },
+  titleText: {
+    alignSelf: 'flex-end',
+    fontSize: 36,
+    fontWeight: 'bold',
+    color: '#4A4A4A',
+    marginRight: '15%',
+  },
+  ddayText: {
+    fontSize: 100,
+    fontWeight: 'bold',
+    color: '#4A4A4A',
+  },
+  dateText: {
+    alignSelf: 'flex-start',
+    fontSize: 21,
+    fontWeight: 'bold',
+    color: '#4A4A4A',
+    marginLeft: '15%',
+  },
+  sendButton: {
+    backgroundColor: 'rgb(97,99,250)',
+    height: 40,
+    width: 50,
+    borderRadius: 20,
+    padding: 5,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginLeft: 5,
+  },
+  chatInput: {
+    backgroundColor: 'white',
+    width: '75%',
+    height: 40,
+    borderWidth: 1,
+    borderColor: '#a5a5a5',
+    borderRadius: 20,
+  },
+  chatControl: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginBottom: 10,
+  },
+  chatScrollView: {
+    width: '90%',
+    margin: 10,
+    alignSelf: 'center',
+    backgroundColor: 'rgba(201,201,201,0.7)',
+    borderRadius: 5,
+    borderWidth: 1,
+    borderColor: '#a5a5a5',
+  },
 });
 
 export default App;
---
+++ Setting.js
@@ -0,0 +1,86 @@
+import React, {useState} from 'react';
+import {
+  View,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+} from 'react-native';
+import DatePicker from 'react-native-date-picker';
+
+const Setting = ({modalHandler, settingHandler}) => {
+  const [ddayInfo, setDdayInfo] = useState({
+    title: '',
+    date: new Date(),
+  });
+
+  return (
+    <View style={styles.container}>
+      <TouchableOpacity
+        style={styles.background}
+        activeOpacity={1}
+        onPress={modalHandler}
+      />
+      <View style={styles.modal}>
+        <Text style={styles.titleText}>설정</Text>
+        <TextInput
+          style={styles.ddayInput}
+          value={ddayInfo.title}
+          onChangeText={changedText =>
+            setDdayInfo(ddatInfo => ({
+              ...ddayInfo,
+              title: changedText,
+            }))
+          }
+          placeholder={'디데이 제목을 입력해주세요.'}
+        />
+        <DatePicker date={ddayInfo.date} mode="date" />
+        <TouchableOpacity
+          onPress={() => settingHandler(ddayInfo.title, ddayInfo.date.getDate())}>
+          <Text style={styles.doneText}>완료</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    position: 'absolute',
+    height: '100%',
+    width: '100%',
+    backgroundColor: 'transparent',
+  },
+  background: {
+    position: 'absolute',
+    height: '100%',
+    width: '100%',
+    backgroundColor: 'rgba(0,0,0,0.5)',
+  },
+  ddayInput: {
+    backgroundColor: 'white',
+    marginBottom: 20,
+    width: '75%',
+    height: 40,
+    borderBottomWidth: 1,
+    borderBottomColor: '#a5a5a5',
+  },
+  modal: {
+    marginHorizontal: 20,
+    borderRadius: 10,
+    alignItems: 'center',
+    marginTop: '50%',
+    backgroundColor: 'white',
+  },
+  doneText: {
+    color: 'rgb(1,123,255)',
+    fontSize: 15,
+    margin: 10,
+  },
+  titleText: {
+    fontSize: 18,
+    margin: 10,
+  },
+});
+
+export default Setting;
